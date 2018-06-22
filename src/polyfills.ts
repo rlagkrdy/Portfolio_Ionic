2e1069f5d92c79caec962ddfd07f6cea4a4d2f0f
rlagkrdy <rlagkrdy3883@naver.com>
2018-06-23 00:58:39 +0900

    ie11 지원 진행중... 일부 css변경 필요...

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -35,7 +35,9 @@ import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
+// Run `npm install --save classlist.js`.
+// 설치완료...
+import 'classlist.js';
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
@@ -45,7 +47,9 @@ import 'core-js/es7/reflect';
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// Run `npm install --save web-animations-js`.
+// 설치완료...
+import 'web-animations-js';
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
